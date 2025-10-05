b7a89cc566a2459d895b783d66aa8dc5fdc29383
suthakaranburaj <suthakaranburaj@gmail.com>
2025-10-06 01:00:21 +0530

    changes

--- server/src/app.js
+++ server/src/app.js
@@ -12,7 +12,12 @@ const app = express();
 
 app.use(
     cors({
-        origin: ["http://localhost:3000","http://localhost:3001"],
+        origin: [
+            "http://localhost:3000",
+            "http://localhost:3001",
+            process.env.CLIENT_PORT,
+            "https://asha-workers-pi.vercel.app/"
+        ],
         credentials: true
     })
 );
